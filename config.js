307a56dfc7478a244da84f420d7f45c7690094e4
Hieu14 <94374529+Hieu14@users.noreply.github.com>
2021-11-15 22:16:33 +0700

    Tang crush

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     btnIntro: 'oke',
     title: 'Cậu yêu tớ có phải không 😙',
     desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là vợ tớ đó nha :> ',
-    btnYes: 'yêu ơi là iuuu <33',
+    btnYes: 'yêu ơi là iuu <33',
     btnNo: 'cậu mơ à :333',
     question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3',
     btnReply: 'Gửi cho tớ <3',
@@ -12,5 +12,5 @@ const CONFIG = {
     mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
     messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: ' https://m.me/hieuu14.nodemy.
+
